fe67984f46199e4edb00355e13df98124a3a0422
Piyush Kumar Sultania <sultania.p@northeastern.edu>
2024-06-23 16:57:06 -0500

    Day-2 of bootcamp

---
+++ section-1/day-1-lesson.py
@@ -0,0 +1,35 @@
+
+# Write your code below this line 👇
+print("A 'single quote' inside a double quote...")
+print('A "double quote" inside a single quote...')
+print("She arrive, said \'Hello\' and left.")
+
+print("Hello World!\nHello World!\nHello World!")
+
+name = input("What is your name? ")
+print("Hello " + name)
+
+print("Hello " + input("What is your name? "))
+
+numOfCharacters = input("What is your name? ")
+print("Length of the word is: " + str(len(numOfCharacters)))
+
+name = "James"
+print(name)
+
+name = "Mike"
+print(name)
+
+# There are two variables, a and b from input
+a = input("Enter number a ")
+b = input("Enter number b ")
+# 🚨 Don't change the code above ☝️
+####################################
+# Write your code below this line 👇
+c = a
+a = b
+b = c
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
