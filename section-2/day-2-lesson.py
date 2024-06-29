b23aabab25c63e08888181c085f2af74744a42f8
Piyush Kumar Sultania <sultania.p@northeastern.edu>
2024-06-28 23:30:09 -0500

    day-2 lesson

---
+++ section-2/day-2-lesson.py
@@ -0,0 +1,38 @@
+# Data Types
+
+# word = "Hello"
+#
+# print(word[0], word[4])
+# print(123_456_678)
+#
+# ###
+# num_char = len(input("What is your name?\n"))
+# print(type(num_char))
+# num_char = str(num_char)
+# print(type(num_char))
+# print("Your name has " + num_char + " characters")
+#
+# num = 65
+# print(float(num))
+
+####
+# two_digit_number = input("Enter the 2 digit number?\n")
+# if len(two_digit_number) != 2:
+#     print('Invalid input')
+# else:
+#     sum_of_digits = int(two_digit_number[0]) + int(two_digit_number[1])
+#     print(sum_of_digits)
+
+# num1 = 10
+# num2 = 5
+# result = num1 // num2
+# print(type(result))
+# print(result)
+
+# height = input("Enter the height?\n")
+# weight = input("Enter the weight?\n")
+#
+# bmi = int(weight) // (float(height) ** 2)
+# print(bmi)
+
+print(round(8 / 3, 2))
